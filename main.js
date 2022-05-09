e16a92ed00ba51c98f1bd4e138f59047e3d0e537
xAbeer <abeerayyad2000@gmail.com>
2022-05-09 17:47:01 +0300

    update api by ajax

--- main.js
+++ main.js
@@ -2,29 +2,28 @@ let test = document.getElementById('test');
 let result = '';
 
 let myHttp = new XMLHttpRequest();
-console.log(myHttp)
 myHttp.onreadystatechange = () => {
     if(myHttp.readyState === 4 && myHttp.status === 200) {
         let data = JSON.parse(myHttp.responseText);
         data = data.articles;
-     
         data.forEach(news => {
             result += `
             <div class='new'>
-            <div class='title-new'>
-                <h3>${news.title}</h3>
-            
-            </div>
+                <div class='title-new'>
+                    <h3>${news.title}</h3>
+                </div>
+
             <div class="content-new">
-            <div class="image">
-            <img src='${news.urlToImage}' >
-            </div>
-            <div class='new-details'>
-            <a href ='${news.url}'>details</a>
-            <span>${news.publishedAt}</span>
-            </div>
-            <p>${news.description}</p>
-            </div>
+
+                    <div class="image">
+                        <img src='${news.urlToImage}' >
+                    </div>
+                    <div class='new-details'>
+                        <a href ='${news.url}'>details</a>
+                        <span>${news.publishedAt}</span>
+                    </div>
+                    <p>${news.description}</p>
+                </div>
             </div>
             `
            
@@ -34,8 +33,10 @@ myHttp.onreadystatechange = () => {
     }
 
 }
+
 myHttp.open('GET','https://newsapi.org/v2/everything?q=tesla&from=2022-04-09&sortBy=publishedAt&apiKey=a7905db5102c46f2870ea52a96ebeefb');
 myHttp.send();
+
 
 let message = ['News World'];
 let speed = 100;
